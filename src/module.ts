8c29d8c4cbd403f1612792307a93026f8a62c5de
Benjamin Brassart <benjaminbrassart@outlook.com>
2023-06-06 10:04:13 +0200

    Update default message

--- src/module.ts
+++ src/module.ts
@@ -3,7 +3,7 @@ It works! This is the default message for ts-template.
 
 Feel free to change anything you need, especially:
  - the package name, in package.json ;
- - the license ;
+ - the license, in package.json and UNLICENSE ;
  - the source files, in src/
 
 Report bugs on GitHub (https://github.com/benjaminbrassart/ts-template/issues)
